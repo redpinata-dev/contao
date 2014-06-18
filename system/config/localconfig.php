60f3c8577a4bc9e8154a9209bb0ed947cefa2b96
Nuebel <n.nuebel@nn-medienagentur.de>
2014-06-18 12:35:44 +0200

    after install

---
+++ system/config/localconfig.php
@@ -0,0 +1,18 @@
+<?php
+
+### INSTALL SCRIPT START ###
+$GLOBALS['TL_CONFIG']['licenseAccepted'] = true;
+$GLOBALS['TL_CONFIG']['installPassword'] = '$6$c5511b75fe4e9a2b$ApxAZS02EVrPC8ONkQ.HYsdq4GqQ3aV4zdPjNWLdVS5cUOIz.iY9KFcdDwcFkDKB0.DJQ93bHnpZtXQAVMkMs0';
+$GLOBALS['TL_CONFIG']['encryptionKey'] = '71c124231648fe9b6eb77b3e3b087895';
+$GLOBALS['TL_CONFIG']['dbDriver'] = 'MySQLi';
+$GLOBALS['TL_CONFIG']['dbHost'] = 'localhost';
+$GLOBALS['TL_CONFIG']['dbUser'] = 'root';
+$GLOBALS['TL_CONFIG']['dbPass'] = 'paula';
+$GLOBALS['TL_CONFIG']['dbDatabase'] = 'contaored';
+$GLOBALS['TL_CONFIG']['dbPconnect'] = false;
+$GLOBALS['TL_CONFIG']['dbCharset'] = 'UTF8';
+$GLOBALS['TL_CONFIG']['dbPort'] = 3306;
+$GLOBALS['TL_CONFIG']['dbSocket'] = '';
+$GLOBALS['TL_CONFIG']['adminEmail'] = 'info@nn-medienagentur.de';
+$GLOBALS['TL_CONFIG']['latestVersion'] = '3.3.2';
+### INSTALL SCRIPT STOP ###
